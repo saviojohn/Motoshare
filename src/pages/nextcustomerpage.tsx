1fb89cdc496f76796647410d949675f52030ff99
savio <savio.john.t@gmail.com>
2023-05-01 20:20:22 +0530

    mapautocomplete

--- src/pages/nextcustomerpage.tsx
+++ src/pages/nextcustomerpage.tsx
@@ -1,84 +1,97 @@
 import React, { useState } from "react";
 import Head from "next/head";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  useMapEvents,
-} from "react-leaflet";
-import { LatLngExpression, LeafletMouseEvent } from "leaflet";
+  GeoapifyGeocoderAutocomplete,
+  GeoapifyContext,
+} from "@geoapify/react-geocoder-autocomplete";
+import "@geoapify/geocoder-autocomplete/styles/minimal.css";
 
-const NextCustomerPage = () => {
-  const [pickUpLocation, setPickUpLocation] =
-    useState<LatLngExpression | null>(null);
-  const [dropOffLocation, setDropOffLocation] =
-    useState<LatLngExpression | null>(null);
+const CustomerLocation = () => {
+  const [pickupLocation, setPickupLocation] = useState({
+    label: "",
+    coordinates: [0, 0] as [number, number],
+  });
+  const [dropLocation, setDropLocation] = useState({
+    label: "",
+    coordinates: [0, 0] as [number, number],
+  });
 
-  const handleMapClick = (event: LeafletMouseEvent) => {
-    const { lat, lng } = event.latlng;
-    if (!pickUpLocation) {
-      setPickUpLocation([lat, lng]);
-    } else if (!dropOffLocation) {
-      setDropOffLocation([lat, lng]);
-    }
-  };
-
-  const MapEventsComponent = () => {
-    useMapEvents({
-      click: handleMapClick,
+  function onPickupSelect(value: {
+    properties: { formatted: any };
+    geometry: { lng: number; lat: number };
+  }) {
+    setPickupLocation({
+      label: value.properties.formatted,
+      coordinates: [value.geometry.lng, value.geometry.lat],
     });
+  }
 
-    return null;
-  };
+  function onDropSelect(value: {
+    properties: { formatted: any };
+    geometry: { lng: number; lat: number };
+  }): void {
+    setDropLocation({
+      label: value.properties.formatted,
+      coordinates: [value.geometry.lng, value.geometry.lat],
+    });
+  }
 
   return (
     <>
       <Head>
-        <title>Next Customer Page</title>
+        <title>Customer Location</title>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha384-zqvGKtOdqDwrH2QoVvKgZNhWCBpxHr0TWX9Rc1BpKcgt7sPZocYInB0VxSy0ca94"
+          crossOrigin=""
+        />
       </Head>
       <div className="container mx-auto px-4 py-6">
-        <h1 className="text-3xl font-bold mb-4">Next Customer Page</h1>
-        <div className="mb-4">
-          <label htmlFor="pickup" className="block font-medium mb-2">
-            Pick-up Location
-          </label>
-          <input
-            id="pickup"
-            type="text"
-            className="w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
-            placeholder="Enter Pick-up Location"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="dropoff" className="block font-medium mb-2">
-            Drop-off Location
-          </label>
-          <input
-            id="dropoff"
-            type="text"
-            className="w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
-            placeholder="Enter Drop-off Location"
-          />
-        </div>
-        <div className="mb-4">
+        <h1 className="text-3xl font-bold mb-4">Customer Location</h1>
+        <form className="max-w-md mx-auto">
+          <div className="mb-4">
+            <label htmlFor="pickup" className="block font-medium mb-2">
+              Pickup Location
+            </label>
+            <GeoapifyContext apiKey="6f0ae9a14f374257b6700c22d4ec7d92">
+              <GeoapifyGeocoderAutocomplete
+                placeholder="Enter pickup location"
+                placeSelect={onPickupSelect}
+              />
+            </GeoapifyContext>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="drop" className="block font-medium mb-2">
+              Drop Location
+            </label>
+            <GeoapifyContext apiKey="6f0ae9a14f374257b6700c22d4ec7d92">
+              <GeoapifyGeocoderAutocomplete
+                placeholder="Enter drop location"
+                placeSelect={onDropSelect}
+              />
+            </GeoapifyContext>
+          </div>
+        </form>
+        <div className="mt-8">
           <MapContainer
-            center={[0, 0]} // Set the initial center of the map
+            center={[51.505, -0.09]}
             zoom={13}
-            style={{ height: "400px", width: "100%" }}
+            style={{ height: "400px" }}
           >
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            {pickUpLocation && (
-              <Marker position={pickUpLocation}>
-                <Popup>Pick-up location</Popup>
+            {pickupLocation.label &&
+              pickupLocation.coordinates.length === 2 && (
+                <Marker position={pickupLocation.coordinates}>
+                  <Popup>{pickupLocation.label}</Popup>
+                </Marker>
+              )}
+            {dropLocation.label && dropLocation.coordinates.length === 2 && (
+              <Marker position={dropLocation.coordinates}>
+                <Popup>{dropLocation.label}</Popup>
               </Marker>
             )}
-            {dropOffLocation && (
-              <Marker position={dropOffLocation}>
-                <Popup>Drop-off location</Popup>
-              </Marker>
-            )}
-            <MapEventsComponent />
           </MapContainer>
         </div>
       </div>
@@ -86,4 +99,4 @@ const NextCustomerPage = () => {
   );
 };
 
-export default NextCustomerPage;
+export default CustomerLocation;
