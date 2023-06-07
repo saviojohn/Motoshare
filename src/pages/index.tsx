242a20b5de3dd3a44d300b704154be92092409e8
savio <savio.john.t@gmail.com>
2023-06-07 18:37:51 +0530

    move

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,21 +9,7 @@ const SelectUserType = () => {
       <Head>
         <title>Select User Type</title>
       </Head>
-      <div className="flex flex-col md:flex-row gap-4">
-        <Link
-          href="/driver"
-          className="bg-yellow-500 text-white px-4 py-2 rounded-md hover:bg-yellow-600"
-        >
-          Bike Registration
-        </Link>
-        <Link
-          href="/customer"
-          className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600"
-        >
-          Customer
-        </Link>
-      </div>
-        <br/>
+
         <div className="flex flex-col md:flex-row gap-4">
             <Link
                 href="/sign_in"
